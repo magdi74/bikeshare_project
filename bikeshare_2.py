38724ba4247f85a091bade5656a7532205e03931
magdi74 <magdim200@gmail.com>
2021-09-24 02:22:54 +0200

    Update bikeshare_2.py

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -74,8 +74,8 @@ def load_data(city, month, day):
 
     Args:
         (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) month - name of the month to filter by, or "none" to apply no month filter
+        (str) day - name of the day of week to filter by, or "none" to apply no day filter
     Returns:
         df - pandas DataFrame containing city data filtered by month and day
     """
